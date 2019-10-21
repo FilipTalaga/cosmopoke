8362364221d24575830c88cc60553703b8186000
Filip Talaga <talaga.filip@gmail.com>
2019-10-21 20:06:15 +0200

    Implemented pokemon by id fetch

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+interface Pokemon {
+    name: string;
+    stats: [];
+    sprites: any;
+}
 
 @Component({
     selector: 'pokemon',
@@ -6,8 +14,16 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./pokemon.component.scss']
 })
 export class PokemonComponent implements OnInit {
-    constructor() { }
+    public pokemon: Pokemon;
+
+    constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
     ngOnInit() {
+        this.activatedRoute.params.subscribe(params => {
+            this.http.get(`https://pokeapi.co/api/v2/pokemon/${params.id}`).subscribe((res: Pokemon) => {
+                this.pokemon = res;
+                console.log(res);
+            });
+        });
     }
 }
