e5bf4e6dce1bf4ec2dc78d3468ee2a3698497381
Filip Talaga <talaga.filip@gmail.com>
2019-10-22 15:43:57 +0200

    Refactored background code

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -16,80 +16,51 @@ export class BackgroundComponent implements OnInit {
     constructor(private renderer: Renderer2) { }
 
     ngOnInit() {
-        this.update();
+        this.update(window.innerWidth, window.innerWidth < window.innerHeight ? window.innerHeight / 2 : window.innerHeight);
     }
 
     @HostListener('window:resize')
     onResize() {
-        this.update();
+        this.update(window.innerWidth, window.innerWidth < window.innerHeight ? window.innerHeight / 2 : window.innerHeight);
     }
 
-    update() {
-        const getTopBackPath = (width, height) => {
-            const x = value => width * value / 950;
-            const y = value => height * value / 500;
+    private update(width: number, height: number) {
+        const x = (value: number) => width * value / 950;
+        const y = (value: number) => height * value / 500;
 
-            return `
-                M ${x(0)}   ${y(0)}
-                L ${x(0)}   ${y(100)}
-                Q ${x(400)} ${y(300)} ${x(700)} ${y(100)}
-                Q ${x(850)} ${y(0)}   ${x(950)} ${y(75)}
-                L ${x(950)} ${y(0)}
-                Z
-            `;
-        };
+        this.setAttr(this.topSvg, 'width', width.toString());
+        this.setAttr(this.topSvg, 'height', height.toString());
+        this.setAttr(this.botSvg, 'width', width.toString());
+        this.setAttr(this.botSvg, 'height', height.toString());
 
-        const getTopFrontPath = (width, height) => {
-            const x = value => width * value / 950;
-            const y = value => height * value / 500;
+        this.setAttr(this.topBack, 'd', `
+            M ${x(0)}   ${y(0)} L ${x(0)}   ${y(100)}
+            Q ${x(400)} ${y(300)} ${x(700)} ${y(100)}
+            Q ${x(850)} ${y(0)}   ${x(950)} ${y(75)}
+            L ${x(950)} ${y(0)} Z
+        `);
+        this.setAttr(this.topFront, 'd', `
+            M ${x(0)}   ${y(0)} L ${x(0)}   ${y(375)}
+            Q ${x(100)} ${y(200)} ${x(250)} ${y(225)}
+            Q ${x(400)} ${y(250)} ${x(500)} ${y(150)}
+            Q ${x(600)} ${y(50)}  ${x(700)} ${y(0)} Z
+        `);
+        this.setAttr(this.botBack, 'd', `
+            M ${x(0)}   ${y(500)} L ${x(0)} ${y(425)}
+            Q ${x(150)} ${y(300)} ${x(350)} ${y(400)}
+            Q ${x(550)} ${y(500)} ${x(950)} ${y(400)}
+            L ${x(950)} ${y(500)} Z
+        `);
+        this.setAttr(this.botFront, 'd', `
+            M ${x(0)}   ${y(500)} L ${x(0)} ${y(475)}
+            Q ${x(150)} ${y(350)} ${x(350)} ${y(425)}
+            Q ${x(550)} ${y(500)} ${x(650)} ${y(350)}
+            Q ${x(750)} ${y(200)} ${x(950)} ${y(300)}
+            L ${x(950)} ${y(500)} Z
+        `);
+    }
 
-            return `
-                M ${x(0)}   ${y(0)}
-                L ${x(0)}   ${y(375)}
-                Q ${x(100)} ${y(200)} ${x(250)} ${y(225)}
-                Q ${x(400)} ${y(250)} ${x(500)} ${y(150)}
-                Q ${x(600)} ${y(50)}  ${x(700)} ${y(0)}
-                Z
-            `;
-        };
-
-        const getBotBackPath = (width, height) => {
-            const x = value => width * value / 950;
-            const y = value => height * value / 500;
-
-            return `
-                M ${x(0)}   ${y(500)}
-                L ${x(0)}   ${y(425)}
-                Q ${x(150)} ${y(300)} ${x(350)} ${y(400)}
-                Q ${x(550)} ${y(500)} ${x(950)} ${y(400)}
-                L ${x(950)} ${y(500)}
-                Z
-            `;
-        };
-
-        const getBotFrontPath = (width, height) => {
-            const x = value => width * value / 950;
-            const y = value => height * value / 500;
-
-            return `
-                M ${x(0)}   ${y(500)}
-                L ${x(0)}   ${y(475)}
-                Q ${x(150)} ${y(350)} ${x(350)} ${y(425)}
-                Q ${x(550)} ${y(500)} ${x(650)} ${y(350)}
-                Q ${x(750)} ${y(200)} ${x(950)} ${y(300)}
-                L ${x(950)} ${y(500)}
-                Z
-            `;
-        };
-
-        this.renderer.setAttribute(this.topSvg.nativeElement, 'width', window.innerWidth.toString());
-        this.renderer.setAttribute(this.topSvg.nativeElement, 'height', window.innerHeight.toString());
-        this.renderer.setAttribute(this.topBack.nativeElement, 'd', getTopBackPath(window.innerWidth, window.innerHeight));
-        this.renderer.setAttribute(this.topFront.nativeElement, 'd', getTopFrontPath(window.innerWidth, window.innerHeight));
-
-        this.renderer.setAttribute(this.botSvg.nativeElement, 'width', window.innerWidth.toString());
-        this.renderer.setAttribute(this.botSvg.nativeElement, 'height', window.innerHeight.toString());
-        this.renderer.setAttribute(this.botBack.nativeElement, 'd', getBotBackPath(window.innerWidth, window.innerHeight));
-        this.renderer.setAttribute(this.botFront.nativeElement, 'd', getBotFrontPath(window.innerWidth, window.innerHeight));
+    private setAttr(el: ElementRef, attr: string, value: string) {
+        this.renderer.setAttribute(el.nativeElement, attr, value);
     }
 }
